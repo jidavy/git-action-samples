1fd4a99f394d5f386193d0414d2dc1f1712c54f8
Babajide <akindelebabajidev@gmail.com>
2026-02-02 03:44:45 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# 1. Look up your specific VPC by ID
-data "aws_vpc" "manual_vpc" {
-  id = "vpc-002855cca3304ddd7"
-}
-
-# 2. Automatically find the subnets that belong to that VPC
-data "aws_subnets" "manual_subnets" {
+# Find subnets inside your specific VPC
+data "aws_subnets" "all" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.manual_vpc.id]
+    values = ["vpc-002855cca3304ddd7"]
   }
 }
 
@@ -28,8 +23,8 @@ resource "aws_instance" "new-node" {
   ami           = "ami-096f46d460613bed4"
   instance_type = "t3.micro"
   
-  # 3. Use the first available subnet found in that VPC
-  subnet_id     = data.aws_subnets.manual_subnets.ids[0]
+  # Grab the first subnet ID found
+  subnet_id     = data.aws_subnets.all.ids[0]
 
   tags = {
     Name = "new-node"
