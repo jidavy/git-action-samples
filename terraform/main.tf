7568d152dad51fa1d0868000366c0de065fbc2b4
Babajide <akindelebabajidev@gmail.com>
2026-02-02 15:56:28 +0000

    Refactor AWS instance settings in main.tf
    Updated AWS instance configuration and removed unused subnet data.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,21 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }   
 }
 
 provider "aws" {
-  region = "eu-west-1"
-}
-
-# Find subnets inside your specific VPC
-data "aws_subnets" "all" {
-  filter {
-    name   = "vpc-id"
-    values = ["vpc-002855cca3304ddd7"]
-  }
+    region = "eu-west-1"
 }
 
 resource "aws_instance" "new-node" {
-  ami           = "ami-096f46d460613bed4"
-  instance_type = "t3.micro"
-  
-  # Grab the first subnet ID found
-  subnet_id     = data.aws_subnets.all.ids[0]
+    ami           = "ami-03e091ef64f3907f8" # Amazon Linux 2 AMI
+    instance_type = "t3.micro"
 
-  tags = {
-    Name = "new-node"
-  }
+    tags = {
+        Name = "New Node"
+    }
 }
